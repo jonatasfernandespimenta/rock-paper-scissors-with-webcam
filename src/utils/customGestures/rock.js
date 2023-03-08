3cb84af76fa414c41f555bd65aba496d8d8757ab
Jonatas Pimenta <jonatas.pimenta@copernico.com.br>
2023-03-08 13:30:49 -0300

    adds readme

--- src/utils/customGestures/rock.js
+++ src/utils/customGestures/rock.js
@@ -4,7 +4,6 @@ const rockGestureDescription = new fp.GestureDescription('rock');
 
 for(let finger of [fp.Finger.Thumb, fp.Finger.Index, fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky]) {
   rockGestureDescription.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
-  rockGestureDescription.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
 }
 
 export default rockGestureDescription;
