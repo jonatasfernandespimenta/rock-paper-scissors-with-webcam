03e9de9c60863e9e228675f5e5e50f1d631f771f
Jonatas Pimenta <jonatas.pimenta@copernico.com.br>
2023-03-08 11:10:49 -0300

    adds a better frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import * as tf from "@tensorflow/tfjs";
+import Lottie from "lottie-react";
 import * as handpose from "@tensorflow-models/handpose";
+import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import { useEffect, useRef, useState } from "react";
 import { drawHand, gameResult, play_options } from "./utils/helpers";
@@ -9,13 +10,19 @@ import rockGestureDescription from "./utils/customGestures/rock";
 import paperGestureDescription from "./utils/customGestures/paper";
 import scissorsDescription from "./utils/customGestures/scissors";
 
+import loadingAnimation from "./assets/loading.json";
+import wonAnimation from "./assets/win.json";
+import loseAnimation from "./assets/lose.json";
+import tieAnimation from "./assets/tie.json";
+
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
   const [playerInput, setPlayerInput] = useState(null);
   const [computerInput, setComputerInput] = useState(null);
-  const [result, setResult] = useState();
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const runHandPose = async () => {
     const net = await handpose.load();
@@ -70,75 +77,195 @@ function App() {
   };
 
   function play() {
-    setComputerInput('');
+    setComputerInput("");
+    setLoading(true);
 
     const random = Math.floor(Math.random() * play_options.length);
     const cpInput = play_options[random];
 
-    setComputerInput(cpInput)
-    setResult(gameResult(cpInput, playerInput))
+    setComputerInput(cpInput);
+
+    setTimeout(() => {
+      setResult(gameResult(cpInput, playerInput));
+    }, 2600);
   }
 
   useEffect(() => {
     runHandPose();
   }, []);
 
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    if (result !== "") {
+      setTimeout(() => {
+        setResult("");
+      }, 2000);
+    }
+  }, [result]);
+
   return (
-    <div className="App">
-      <Webcam
-        ref={webcamRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480,
-        }}
-      />
+    <>
+      {loading ? (
+        <div
+          style={{
+            background: "#00000040",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            zIndex: 20,
+          }}
+        >
+          <Lottie
+            animationData={loadingAnimation}
+            autoPlay
+            speed={1}
+            loop={false}
+          />
+        </div>
+      ) : null}
 
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zindex: 9,
-          width: 640,
-          height: 480,
-          background: 'black'
-        }}
-      />
+      {result === "won" ? (
+        <div className="lottie">
+          <Lottie
+            animationData={wonAnimation}
+            autoPlay
+            speed={1}
+            loop={false}
+          />
+        </div>
+      ) : null}
 
-      <h1
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 400,
-          bottom: 500,
-          right: 0,
-          textAlign: "center",
-          height: 100,
-          color: 'white'
-        }}
-      >
-        {result}
-      </h1>
+      {result === "tie" ? (
+        <div className="lottie">
+          <Lottie
+            animationData={tieAnimation}
+            autoPlay
+            speed={1}
+            loop={false}
+          />
+        </div>
+      ) : null}
 
-      <button style={{ marginTop: '700px', width: '50px' }} onClick={play}>Play</button>
-      
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <p>Computador: {computerInput}</p>
-        <p>Resultado: {result}</p>
+      {result === "lost" ? (
+        <div className="lottie">
+          <Lottie
+            animationData={loseAnimation}
+            autoPlay
+            speed={1}
+            loop={false}
+          />
+        </div>
+      ) : null}
+
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            padding: 10,
+            marginTop: 60,
+          }}
+        >
+          <div
+            style={{
+              zIndex: 10,
+              borderRadius: 300,
+              background: "#f9ae1c",
+              width: 120,
+              height: 120,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: "700px",
+              marginTop: "-60px",
+              position: "absolute",
+            }}
+          >
+            <img src="/user_pfp.png" width={100} />
+          </div>
+
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 20,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
+
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 20,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+              background: "#fcbc3f",
+            }}
+          />
+
+          <div
+            style={{
+              zIndex: 10,
+              borderRadius: 300,
+              background: "#bf5842",
+              width: 120,
+              height: 120,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "700px",
+              marginTop: "-60px",
+            }}
+          >
+            <img src="/bot_pfp.png" width={100} />
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              right: 20,
+              display: "flex",
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+              justifyContent: "center",
+              alignItems: "center",
+              background: "#bf7262",
+            }}
+          >
+            {computerInput && !loading ? (
+              <img src={`/${computerInput}.png`} />
+            ) : null}
+          </div>
+        </div>
+
+        <button style={{ marginTop: "430px" }} onClick={play}>
+          Play
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
